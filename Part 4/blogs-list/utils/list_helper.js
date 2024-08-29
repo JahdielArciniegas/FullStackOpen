a67329eb3ff3b93715ae09b47b88e574ac95f2eb
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2024-08-29 14:49:00 -0400

    Exercise 4.4

--- Part 4/blogs-list/utils/list_helper.js
+++ Part 4/blogs-list/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (listBlog) => {
+  const likes = listBlog.map(blog => blog.likes)
+  
+  let initial = 0
+  const sumTotalLikes = likes.reduce((acc, cur) => acc + cur,initial)
+  return sumTotalLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
