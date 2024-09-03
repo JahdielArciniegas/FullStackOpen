2fa960d149be04507f0072573f9153d7aaa43942
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2024-09-03 12:08:40 -0400

    Exercise 18

--- Part 4/blogs-list/app.js
+++ Part 4/blogs-list/app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -21,5 +22,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
