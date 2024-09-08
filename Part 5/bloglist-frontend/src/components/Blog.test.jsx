76de2d78e749298c8dba60a24f4d1a1b25311e62
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2024-09-07 22:07:48 -0400

    Exercise 5.14

--- Part 5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part 5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect } from 'vitest'
+import { beforeEach, describe, expect } from 'vitest'
 
-test('Title and Author visibles, but url and likes not', async() => {
+describe('<Blog/>', () => {
+  let container
+
   const blog = {
     title : 'Understanding JavaScript Closures',
     author : 'Jane Doe',
@@ -20,34 +23,27 @@ test('Title and Author visibles, but url and likes not', async() => {
     name : 'Matti Luukkainen'
   }
 
-  const { container } = render(<Blog blog={blog} user={user} addLikes={addLikes} deleteBlog={deleteBlog}/>)
 
-  const title = screen.getByText('Understanding JavaScript Closures - Jane Doe')
-  expect(title).toBeDefined()
+  beforeEach(() => {
+    container = render(<Blog blog={blog} user={user} addLikes={addLikes} deleteBlog={deleteBlog}/>).container
+  })
 
-  screen.debug(container)
+  test('Title and Author visibles, but url and likes not', async() => {
+    const title = screen.getByText('Understanding JavaScript Closures - Jane Doe')
+    expect(title).toBeDefined()
+    screen.debug(container)
+    const hide = container.querySelector('.content-hide')
+    expect(hide).toHaveStyle('display: none')
+  })
 
-  const hide = container.querySelector('.content-hide')
-  expect(hide).toHaveStyle('display: none')
+  test('After clicking the view button, view all details', async() => {
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+    const div = container.querySelector('.content-hide')
+    expect(div).not.toHaveStyle('display : none')
+  } )
 })
 
-test('content hide pass view', async() => {
-  const blog = {
-    title : 'Understanding JavaScript Closures',
-    author : 'Jane Doe',
-    url : 'https://example.com/javascript-closures',
-    likes : 252,
-    user : {
-      name : 'Matti Luukkainen'
-    }
-  }
 
-  const addLikes = () => console.log('likes')
-  const deleteBlog = () => console.log('delete')
 
-  const user = {
-    name : 'Matti Luukkainen'
-  }
-
-  render(<Blog blog={blog} addLikes={addLikes} deleteBlog={deleteBlog} user={user}/>)
-} )
